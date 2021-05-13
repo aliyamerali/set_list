9434f0122e7eee998df8061531925bebf9c586e0
Aliya Merali <merali4@gmail.com>
2021-05-12 19:27:15 -0600

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+@prince = Artist.create!(name: "Prince")
+@purple = @prince.songs.create!(title: "Purple Rain", length: 452, play_count: 3451)
+@beret = @prince.songs.create!(title: "Raspberry Beret", length: 234, play_count: 4567)
